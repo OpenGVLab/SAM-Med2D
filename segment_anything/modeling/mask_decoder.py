3058db3baeefe5c5d80c2d84b0dfdc7ba8509571
Ningrong Ye <66538912+nye0@users.noreply.github.com>
2023-09-02 15:56:04 +0800

    adapt for personalize-SAM

--- segment_anything/modeling/mask_decoder.py
+++ segment_anything/modeling/mask_decoder.py
@@ -75,6 +75,8 @@ class MaskDecoder(nn.Module):
         sparse_prompt_embeddings: torch.Tensor, #[B, 3, 256]
         dense_prompt_embeddings: torch.Tensor,  #[B, 256, 64, 64]
         multimask_output: bool,
+        attn_sim=None,
+        target_embedding=None
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         """
         Predict masks given image and prompt embeddings.
@@ -97,6 +99,8 @@ class MaskDecoder(nn.Module):
             image_pe=image_pe,
             sparse_prompt_embeddings=sparse_prompt_embeddings,
             dense_prompt_embeddings=dense_prompt_embeddings,
+            attn_sim=attn_sim,
+            target_embedding=target_embedding
         )
 
         # Select the correct mask or masks for output
@@ -116,6 +120,8 @@ class MaskDecoder(nn.Module):
         image_pe: torch.Tensor,
         sparse_prompt_embeddings: torch.Tensor,
         dense_prompt_embeddings: torch.Tensor,
+        attn_sim=None,
+        target_embedding=None
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         """Predicts masks. See 'forward' for more details."""
         # Concatenate output tokens
@@ -132,7 +138,7 @@ class MaskDecoder(nn.Module):
         b, c, h, w = src.shape
 
         # Run the transformer
-        hs, src = self.transformer(src, pos_src, tokens)
+        hs, src = self.transformer(src, pos_src, tokens, attn_sim, target_embedding)
         iou_token_out = hs[:, 0, :]
         mask_tokens_out = hs[:, 1 : (1 + self.num_mask_tokens), :]
 
